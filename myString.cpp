80cd3980826f207b44843159453d86486a0523d3
DoubleSpicy <khhojohn0520@gmail.com>
2025-01-07 22:58:57 +0800

    use LSB as long-short flag

--- myString.cpp
+++ myString.cpp
@@ -15,24 +15,30 @@ class _longStr {
 
 public:
     _longStr(){
-        _size = 0;
-        _cap = 0b1111; // 8 with 1 LSB as flag
-        _data = new char[8];
+        init();
     };
 
     _longStr(char *val){
         _size = strlen(val);
-        _cap = _size+1;
-        _data = new char[_cap];
-        memcpy(_data, val, _size);
+        _data = new char[_size+1];
+        memcpy(_data, val, _size+1);
         _data[_size+1] = '\0';
+        saveCap(_size+1);
+    }
+
+    constexpr void init(){
+        _size = 0;
+        _cap = 0b1111; // 8 with 1 LSB as flag
+        _data = new char[8];
+        _data[0] = '\0';
     }
     
-    size_t size(){
+    inline size_t size(){
         return _size;
     }
-    void operator+=(char c){
-        if(_cap - _size <= 1){
+
+    inline void operator+=(char c){
+        if(getCap() - _size <= 1){
             resize(2*_size);
         }
         _size++;
@@ -40,53 +46,87 @@ public:
         _data[_size] = '\0';
     }
 
-    void operator+=(char* c){
+    inline void operator+=(char* c){
         auto newSize = strlen(c) + _size;
-        if(_cap - newSize <= 1){
+        if(getCap() - newSize <= 1){
            resize(newSize*2);
         }
         memcpy(_data+_size,c,strlen(c));
         _size = newSize;
         _data[_size] = '\0';
     }
+    
 
-    auto raw(){
+    inline auto raw(){
         return _data;
+    }
+
+    inline void clear(){
+        // clean all contents to default state
+        delete [] _data;
+        init();
     }
 
 
 private:
-    void resize(size_t newSize){
+    inline void resize(size_t newSize){
         assert(newSize > 0);
-        auto newCap = newSize+1;
         auto newData = new char[newSize+1]; // always reserve 1 more byte for \0
         // copy current data to new
         auto sizeToCopy = min(_size, newSize); // extend or shrink the data
         memcpy(newData, _data, sizeToCopy);
         newData[sizeToCopy+1] = '\0';
-        swap(_cap, newSize);
+        saveCap(newSize+1);
         swap(_data, newData);
         delete [] newData;
+    }
+
+    inline size_t getCap(){
+        return _cap >> 1;
+    }
+
+    inline void saveCap(size_t num){
+        // store a 31-bit number with LSB flag = 0b1
+        constexpr size_t limit = (~0b0) - 1;
+        assert(num <= limit);
+        num <<= 1;
+        num |= 0b1;
+        _cap = num;
     }
 };
 
 
 
-// class _shortStr{
-//     char data[24]; // 1st byte as type flag
-//     public:
-//         size_t size(){
-//             return strlen(data);
-//         }
-//         void operator+=(const char & c){
-//             auto curSize = size();
-//             data[curSize+1] = c;
-//             data[curSize+2] = '\0';
-//         }
-//         char* raw(){
-//             return &data[1];
-//         }
-// };
+class _shortStr{
+    char data[24]; // 1st byte as type flag
+public:
+        _shortStr(){
+            init();
+        }
+        _shortStr(){
+
+        }
+        inline size_t size(){
+            return strlen(raw());
+        }
+        inline void operator+=(char c){
+            assert(size() < 23);
+            auto curSize = size();
+            raw()[curSize+1] = c;
+            raw()[curSize+2] = '\0';
+        }
+        inline char* raw(){
+            return &data[1];
+        }
+        inline void clear(){
+            init();
+        }
+private:
+    constexpr void init(){
+        data[0] = 0b0;
+        data[1] = '\0';
+    }
+};
 
 // union _strData{
 //     _longStr longStr;
@@ -139,15 +179,34 @@ std::ostream& operator<<(std::ostream& os, _longStr & s ){
 }
 
 
-int main(){
-    char *word = "abcdefg";
+void testLongStr(){
+    char *word = "abcdefg"; // abcdefg
     _longStr ls(word);
     cout << ls << " " << ls.size() <<  endl;
     ls += 'h';
-    cout << ls << " after " << ls.size() << " " << strlen(ls.raw()) << endl;
+    cout << ls << " after " << ls.size() << " " << strlen(ls.raw()) << endl; // abcdefgh
 
     char *word2 = "123456";
     ls += word2;
-    cout << ls << " after3 " << ls.size() << " " << strlen(ls.raw()) << endl;
-    // 
+    cout << ls << " after3 " << ls.size() << " " << strlen(ls.raw()) << endl; // abcdefgh123456
+
+    ls.clear();
+    cout << ls << " after clear " << ls.size() << " " << strlen(ls.raw()) << endl; // empty string
+
+    int multiplyHowManyTimes = 3;
+    while(multiplyHowManyTimes--){
+        ls += "123456|";
+    }
+    cout << ls << " after add " << ls.size() << " " << strlen(ls.raw()) << endl; // 123456|123456|123456|
+
+    cout << "sizeof ls = " << sizeof(ls) << '\n';
+}
+
+void testShortStr(){
+    
+}
+
+int main(){
+    testLongStr();
+    testShortStr();
 }
